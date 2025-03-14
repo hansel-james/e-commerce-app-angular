3639fa2e8cadcd074c81bfd86da4ae39dca89456
hansel-james <hanseljames10@gmail.com>
2025-03-14 10:54:00 +0530

    made components more mobile friendly

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -24,26 +24,35 @@ export class HomeComponent implements AfterViewInit, AfterViewChecked {
       const textElement = textElementRef.nativeElement;
       const val = parseFloat(textElement.getAttribute('data-val') || '50');
 
-      const moveAway = (event: MouseEvent) => {
+      const moveAway = (event: MouseEvent | TouchEvent) => {
         const rect = textElement.getBoundingClientRect();
-        const offsetX = event.clientX - rect.left - rect.width / 2;
-        const offsetY = event.clientY - rect.top - rect.height / 2;
+        let clientX, clientY;
+
+        if (event instanceof MouseEvent) {
+          clientX = event.clientX;
+          clientY = event.clientY;
+        } else {
+          clientX = event.touches[0].clientX;
+          clientY = event.touches[0].clientY;
+        }
+
+        const offsetX = clientX - rect.left - rect.width / 2;
+        const offsetY = clientY - rect.top - rect.height / 2;
         const moveX = (offsetX / rect.width) * val;
         const moveY = (offsetY / rect.height) * val;
 
-        this.renderer.setStyle(
-          textElement,
-          'transform',
-          `translate(${moveX}px, ${moveY}px)`
-        );
+        this.renderer.setStyle(textElement, 'transform', `translate(${moveX}px, ${moveY}px)`);
       };
 
       const resetPosition = () => {
         this.renderer.setStyle(textElement, 'transform', 'translate(0, 0)');
       };
 
+      // Listen for both mouse and touch events
       this.renderer.listen(textElement, 'mousemove', moveAway);
       this.renderer.listen(textElement, 'mouseleave', resetPosition);
+      this.renderer.listen(textElement, 'touchstart', moveAway);
+      this.renderer.listen(textElement, 'touchend', resetPosition);
     });
   }
 }
